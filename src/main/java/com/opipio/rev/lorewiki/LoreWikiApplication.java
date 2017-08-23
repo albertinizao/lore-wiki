f137f58cc358596a9bf0d4dbfa8b3b44be352ac5
acebrian <acebrian@paradigmadigital.com>
2017-08-23 21:11:22 +0200

    initial

---
+++ src/main/java/com/opipio/rev/lorewiki/LoreWikiApplication.java
@@ -0,0 +1,12 @@
+package com.opipio.rev.lorewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoreWikiApplication.class, args);
+	}
+}
